8780ee6618e485f101654b3fadd3258d2b46ed2d
tony lombardi <alombardi.331@gmail.com>
2021-01-27 01:31:48 -0500

    add css to js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import App from 'next/app';
 import MainLayout from '../components/Layouts/main';
 import '../styles/index.css';
-import '../styles/pages/main.css'
+import '../styles/pages/main.css';
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+        <style jsx global>{`
+          body {
+            margin: 0 !important;
+            overflow: hidden;
+          }
+        `}</style>
+      </>
     );
   }
 }
